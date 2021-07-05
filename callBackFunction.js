798e805b36e10ab7adc0855d41a6d3f839c75bbe
MD.Omar Faruq <omarfaruq.bplus.6600@gmail.com>
2021-07-05 21:33:02 +0600

    callbackfunction

---
+++ callBackFunction.js
@@ -0,0 +1,27 @@
+// callback function
+
+var arr= [1,2,3,4,5,6,7,8,9];
+arr.forEach(function(a){
+    console.log(a);
+})
+
+
+console.log("........../ another way / ........");
+
+function cbf(a){
+    console.log(a);
+}
+arr.forEach(cbf);
+
+
+
+console.log("......../ return the function /......");
+
+function A(name){
+    return function(){
+        return "my name is "+ name;
+    }
+}
+
+var fullName= A("omar Faruq")();
+console.log(fullName);
