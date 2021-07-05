461e6c40e1081060db8de19e6052b64547d018e4
MD.Omar Faruq <omarfaruq.bplus.6600@gmail.com>
2021-07-05 17:56:20 +0600

    p2

--- p2.js
+++ p2.js
@@ -1,8 +1,40 @@
 // nested function....
 
-function myFather(father){
-    function me(faruq){
 
+
+
+function outer(){
+
+    function inner(){
+        console.log("this is inner function");
+     
     }
-    return
+inner();
+ console.log("this is outer function");
 }
+
+outer();
+
+
+
+function math(a, b){
+
+    function aa(){
+        return a+b;
+    }
+
+    function bb(){
+        return a-b;
+    }
+    function cc(){
+        return a*b;
+    }
+    function dd(){
+        return a/b;
+    }
+
+    return  aa() + bb() + cc() + dd();
+}
+
+var result = math(5,5);
+console.log(result);
